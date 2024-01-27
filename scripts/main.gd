9548280a8d2bb4875d236c50f6d41af222a84514
Martin Rouault <mrouault@hotmail.fr>
2024-01-27 18:03:24 +0100

    Stun player

--- scripts/main.gd
+++ scripts/main.gd
@@ -18,3 +18,5 @@ func _on_dialog(_profile, _joke):
 func _on_end_dialog():
 	$Level/Player/Camera2D.enabled = true
 	$Level.get_tree().paused = false
+	$Level/Player.set_physics_process(false)
+	get_tree().create_timer(0.5).timeout.connect(func():$Level/Player.set_physics_process(true))
