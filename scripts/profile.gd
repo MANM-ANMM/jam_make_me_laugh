969341f97db0cc046f1695dc41890fca240336c7
Silver-Ink <martin.74@orange.fr>
2024-01-27 16:27:01 +0100

    le personnage peut parler

--- scripts/profile.gd
+++ scripts/profile.gd
@@ -5,12 +5,19 @@ enum EYE_POS {Right, Left, Center}
 #childs
 @onready var node_face = $Face
 @onready var node_pupils = $Pupils
+@onready var node_mouth_s = $MouthS
+@onready var node_mouth_o = $MouthO
+
 
 var eye_dest := Vector2.ZERO
 var eye_pos := Vector2.ZERO
 
+var sentence := ""
+var word_progress := speak_speed
+
 @export var eye_shaking : int = 0
 @export_range(0, 100) var eye_radius : int = 10
+@export var speak_speed := .07
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,6 +35,13 @@ func look_toward(pos : EYE_POS):
 			_set_from_angle(PI)
 		EYE_POS.Center:
 			eye_dest = Vector2.ZERO
+			
+func speak(tone_sequence : String):
+	sentence = tone_sequence.reverse()
+	
+func _set_mouth_pos(open : bool):
+	node_mouth_o.visible = open
+	node_mouth_s.visible = !open
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -40,7 +54,15 @@ func _process(delta):
 	else:
 		node_pupils.position = eye_pos
 		
-		
+	if (sentence != ""):
+		word_progress -= delta
+		if (word_progress <= 0):
+			word_progress = speak_speed
+			_set_mouth_pos(sentence.right(1) == "O")
+			sentence = sentence.erase(sentence.length()-1)
+	else:
+		_set_mouth_pos(false)
+			
 		
 		
 		
