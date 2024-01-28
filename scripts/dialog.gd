a934a4c6b7330662086c332efa17bc8dc95f8bc7
Silver-Ink <martin.74@orange.fr>
2024-01-28 15:44:03 +0100

    tous les portraits fonctionnent

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -8,39 +8,64 @@ var joke
 func _ready():
 	$CanvasLayer/Skip.visible = false
 	$CanvasLayer/Joke.text = joke
-	$Profile.speak("oOoOoOoooOOOOoooooOOoOOOOoOOOoOOO")
+	$Profile.speak("oOoOoOoooOOOOoooooOOoOOOOoOOOoOOOoOoOoOOOOOoOOooooOooooOoOOOoOOOoooOooOOooooOOooooOOOOoooOOOOooooOOoOOoOOOOoooOOOoooOOoo")
 	
 func init():
 	character = $Profile
 	match profile:
 		'Geek':
-			character.face = preload("res://assets/sprites/profils/geek_base.png")
-			character.pupils = null
-			character.mouth_o = preload("res://assets/sprites/profils/geek_mouth_o.png")
-			character.mouth_s = preload("res://assets/sprites/profils/geek_mouth_s.png")
-			character.position = Vector2(20, -13)
-			character.scale = Vector2(0.6, 0.6)
+			if (randi_range(0, 1) == 0):
+				character.face = preload("res://assets/sprites/profils/geek_base.png")
+				character.pupils = null
+				character.mouth_o = preload("res://assets/sprites/profils/geek_mouth_o.png")
+				character.mouth_s = preload("res://assets/sprites/profils/geek_mouth_s.png")
+				character.position = Vector2(20, -13)
+				character.scale = Vector2(0.6, 0.6)
+			else:
+				character.face = preload("res://assets/sprites/profils/geek2_base.png")
+				character.pupils = null
+				character.mouth_o = preload("res://assets/sprites/profils/geek2_mouth_o.png")
+				character.mouth_s = null
+				character.position = Vector2(15, 71)
+				character.scale = Vector2(0.362, 0.362)
+				
 		'Martin':
-			character.face = preload("res://assets/sprites/profils/joggeuse_base.png")
-			character.pupils = null
-			character.mouth_o = preload("res://assets/sprites/profils/joggeuse_mouth_o.png")
-			character.mouth_s = preload("res://assets/sprites/profils/joggeuse_mouth_s.png")
-			character.position = Vector2(17, 7)
-			character.scale = Vector2(0.427, .427)
+			if (randi_range(0, 1) == 0):
+				character.face = preload("res://assets/sprites/profils/joggeuse_base.png")
+				character.pupils = null
+				character.mouth_o = preload("res://assets/sprites/profils/joggeuse_mouth_o.png")
+				character.mouth_s = preload("res://assets/sprites/profils/joggeuse_mouth_s.png")
+				character.position = Vector2(17, 7)
+				character.scale = Vector2(0.427, .427)
+			else:
+				character.face = preload("res://assets/sprites/profils/etudiante_base.png")
+				character.pupils = null			
+				character.mouth_o = null
+				character.mouth_s = preload("res://assets/sprites/profils/etudiante_mouth_s.png")
+				character.position = Vector2(15, 71)
+				character.scale = Vector2(0.362, 0.362)
 		'Lilian':
-			character.face = preload("res://assets/sprites/profils/profil1_base.png")
-			character.pupils = null
-			character.mouth_o = preload("res://assets/sprites/profils/profil1_mouth_o.png")
-			character.mouth_s = preload("res://assets/sprites/profils/profil1_mouth_s.png")
-			character.position = Vector2(26, 36)
-			character.scale = Vector2(1.68, 1.68)
+			character.face = preload("res://assets/sprites/profils/blaze_base.png")
+			character.pupils = null			
+			character.mouth_o = null
+			character.mouth_s = null
+			character.position = Vector2(37, 52)
+			character.scale = Vector2(0.456, 0.456)
 		'Mamie':
-			character.face = preload("res://assets/sprites/profils/mamie_base.png")
-			character.pupils = null
-			character.mouth_o = preload("res://assets/sprites/profils/mamie_mouth_o.png")
-			character.mouth_s = preload("res://assets/sprites/profils/mamie_mouth_s.png")
-			character.position = Vector2(26, 36)
-			character.scale = Vector2(1.68, 1.68)
+			if (randi_range(0,1) == 0):
+				character.face = preload("res://assets/sprites/profils/mamie_base.png")
+				character.pupils = null
+				character.mouth_o = preload("res://assets/sprites/profils/mamie_mouth_o.png")
+				character.mouth_s = preload("res://assets/sprites/profils/mamie_mouth_s.png")
+				character.position = Vector2(26, 36)
+				character.scale = Vector2(1.68, 1.68)
+			else:
+				character.face = preload("res://assets/sprites/profils/collegien_base.png")
+				character.pupils = null			
+				character.mouth_o = preload("res://assets/sprites/profils/collegien_mouth_o.png")
+				character.mouth_s = null
+				character.position = Vector2(10, 107)
+				character.scale = Vector2(.277, .277)
 	print(character.face)
 	character.init_sprites()
 
