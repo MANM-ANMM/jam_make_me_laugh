351adf5411337272e85c288cd1fb18276a509e49
Silver-Ink <martin.74@orange.fr>
2024-01-28 12:04:44 +0100

    sprite de dialogue selon le perso

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -3,11 +3,32 @@ extends Node2D
 var skippable = false
 var profile
 var joke
+@onready var character := $Profile
 
 func _ready():
 	$CanvasLayer/Skip.visible = false
 	$CanvasLayer/Joke.text = joke
 	$Profile.speak("oOoOoOoooOOOOoooooOOoOOOOoOOOoOOO")
+	
+func init():
+	character = $Profile
+	match profile:
+		'Geek':
+			character.face = preload("res://assets/sprites/profils/geek_base.png")
+			character.pupils = null
+			character.mouth_o = preload("res://assets/sprites/profils/geek_mouth_o.png")
+			character.mouth_s = preload("res://assets/sprites/profils/geek_mouth_s.png")
+		'Martin':
+			pass
+		'Lilian':
+			pass
+		'Mamie':
+			character.face = preload("res://assets/sprites/profils/mamie_base.png")
+			character.pupils = null
+			character.mouth_o = preload("res://assets/sprites/profils/mamie_mouth_o.png")
+			character.mouth_s = preload("res://assets/sprites/profils/mamie_mouth_s.png")
+	print(character.face)
+	character.init_sprites()
 
 func _update_face(_delta):
 	if Input.is_action_just_pressed("move_left"):
@@ -23,7 +44,8 @@ func _update_face(_delta):
 			$Profile.eye_shaking = 0
 
 	if skippable and Input.is_action_just_pressed("interaction"):
-		$Profile.flee(Profil.FLEE_DIR.Down)
+		var c = randi_range(0, 2)
+		$Profile.flee(c)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
